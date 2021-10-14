db847f47ca70e7265b5e40d2789681979bb3cce7
Vanya <heyhello2031@gmail.com>
2021-10-14 15:34:08 +0300

    initial project commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
+
+class MainWidget(Widget):
+    pass
+
+class BoxLayoutExample(BoxLayout):
+    pass
+
+class FirstApp(App):
+    pass
+
+FirstApp().run()
